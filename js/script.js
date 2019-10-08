3630471d9dcc379ef32c74fa0bd8c41312b81941
MichalOciepa <ociep15@gmail.com>
2019-10-08 13:13:36 +0200

    add animation

--- js/script.js
+++ js/script.js
@@ -20,24 +20,24 @@ ambulance.addEventListener("click", function () {
 })
 
 //Hobby details
-const paramedic = document.querySelector(".hbb1");
-const coding = document.querySelector(".hbb2");
-const arduino = document.querySelector(".hbb3");
-const triathlon = document.querySelector(".hbb4");
+// const paramedic = document.querySelector(".hbb1");
+// const coding = document.querySelector(".hbb2");
+// const arduino = document.querySelector(".hbb3");
+// const triathlon = document.querySelector(".hbb4");
 
 
-paramedic.addEventListener("click", function () {
-    paramedic.classList.toggle("opacity");
-})
-coding.addEventListener("click", function () {
-    coding.classList.toggle("opacity");
-})
-arduino.addEventListener("click", function () {
-    arduino.classList.toggle("opacity");
-})
-triathlon.addEventListener("click", function () {
-    triathlon.classList.toggle("opacity");
-})
+// paramedic.addEventListener("click", function () {
+//     paramedic.classList.toggle("opacity");
+// })
+// coding.addEventListener("click", function () {
+//     coding.classList.toggle("opacity");
+// })
+// arduino.addEventListener("click", function () {
+//     arduino.classList.toggle("opacity");
+// })
+// triathlon.addEventListener("click", function () {
+//     triathlon.classList.toggle("opacity");
+// })
 
 //Scroll into html
 function scroll(e) {
@@ -54,4 +54,3 @@ function scroll(e) {
 
 };
 $('a[href^="#"]').click(scroll);
-//Show animation after scroll
